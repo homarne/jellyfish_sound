0f9137694abd775a0557e5c551c59f9ce0722dda
homarne workshop <homarne@gmail.com>
2019-02-02 10:35:39 -0800

    added signal level transmit when talking

--- jellyfish_sound_2.ino
+++ jellyfish_sound_2.ino
@@ -25,23 +25,46 @@
 //
 // This example code is in the public domain.
 
+#include "audio_files.h"
+
 #include <Audio.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
 #include <SerialFlash.h>
 
-#include "audio_files.h"
+#define RMS_REPORT
 
-AudioPlaySdWav           playWav1;
+#ifndef RMS_REPORT
+AudioPlaySdWav           playSdWav1;
 // Use one of these 3 output types: Digital I2S, Digital S/PDIF, or Analog DAC
 AudioOutputI2S           audioOutput;
 //AudioOutputSPDIF       audioOutput;
 //AudioOutputAnalog      audioOutput;
-AudioConnection          patchCord1(playWav1, 0, audioOutput, 0);
-AudioConnection          patchCord2(playWav1, 1, audioOutput, 1);
+AudioConnection          patchCord1(playSdWav1, 0, audioOutput, 0);
+AudioConnection          patchCord2(playSdWav1, 1, audioOutput, 1);
 AudioControlSGTL5000     sgtl5000_1;
+#endif
 
+#ifdef RMS_REPORT
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioPlaySdWav           playSdWav1;     //xy=207,225
+AudioAnalyzeRMS          rms1;           //xy=364,268
+AudioAnalyzeRMS          rms2; //xy=366,176
+AudioOutputI2S           i2s1;           //xy=504,224
+AudioConnection          patchCord1(playSdWav1, 0, rms2, 0);
+AudioConnection          patchCord2(playSdWav1, 0, i2s1, 0);
+AudioConnection          patchCord3(playSdWav1, 1, rms1, 0);
+AudioConnection          patchCord4(playSdWav1, 1, i2s1, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=202,189
+// GUItool: end automatically generated code
+#endif
 
 // Use these with the Teensy Audio Shield
 #define SDCARD_CS_PIN    10
@@ -52,8 +75,12 @@ AudioControlSGTL5000     sgtl5000_1;
 #define SPEAKING 23
 #define LISTENING 37
 
-#define MODE_MOON
-//#define MODE_JELLY
+#define SIG_LEVEL_MIN 64
+#define SIG_LEVEL_MAX 96
+#define SIG_LEVEL_RANGE SIG_LEVEL_MAX - SIG_LEVEL_MIN
+
+//#define MODE_MOON
+#define MODE_JELLY
 
 int theme_step = 0;
 int ambient_step = 0;
@@ -80,7 +107,7 @@ void setup() {
 
   // Audio connections require memory to work.  For more
   // detailed information, see the MemoryAndCpuUsage example
-  AudioMemory(8);
+  AudioMemory(12);
 
   // Comment these out if not using the audio adaptor board.
   // This may wait forever if the SDA & SCL pins lack
@@ -109,18 +136,61 @@ void playFile(const char *filename)
 
   // Start playing the file.  This sketch continues to
   // run while the file plays.
-  playWav1.play(filename);
+  playSdWav1.play(filename);
 
   // A brief delay for the library read WAV info
   delay(5);
-
+  
   // Simply wait for the file to finish playing.
-  while (playWav1.isPlaying()) {
+  while (playSdWav1.isPlaying()) {
     // uncomment these lines if you audio shield
     // has the optional volume pot soldered
     //float vol = analogRead(15);
     //vol = vol / 1024;
     // sgtl5000_1.volume(vol);
+    
+//    if (rms1.available()) {
+//      int signal_level = 0;
+//      signal_level = rms1.read() * 100.0;
+//      //Serial.print("signal level ");
+//      //Serial.println(signal_level);
+//      if (signal_level > SIG_LEVEL_RANGE){signal_level = SIG_LEVEL_RANGE;}
+//      Serial1.write(signal_level + SIG_LEVEL_MIN);
+//    }
+//    delay(100);
+    
+  }
+}
+
+void playTalkFile(const char *filename)
+{
+  Serial.print("Playing file: ");
+  Serial.println(filename);
+
+  // Start playing the file.  This sketch continues to
+  // run while the file plays.
+  playSdWav1.play(filename);
+
+  // A brief delay for the library read WAV info
+  delay(5);
+  
+  // Simply wait for the file to finish playing.
+  while (playSdWav1.isPlaying()) {
+    // uncomment these lines if you audio shield
+    // has the optional volume pot soldered
+    //float vol = analogRead(15);
+    //vol = vol / 1024;
+    // sgtl5000_1.volume(vol);
+    if (rms1.available()) {
+      int signal_level = 0;
+      signal_level = rms1.read() * 100.0;
+      //Serial.print("signal level ");
+      //Serial.println(signal_level);
+      if (signal_level > SIG_LEVEL_RANGE){signal_level = SIG_LEVEL_RANGE;}
+      Serial1.write(signal_level + SIG_LEVEL_MIN);
+    }
+    delay(100);
+    
   }
 }
 
@@ -131,7 +201,7 @@ int playFileInterruptable(const char *filename)
 
   // Start playing the file.  This sketch continues to
   // run while the file plays.
-  playWav1.play(filename);
+  playSdWav1.play(filename);
 
   // A brief delay for the library read WAV info
   delay(5);
@@ -141,7 +211,7 @@ int playFileInterruptable(const char *filename)
   
   // Wait for the file to finish playing 
   // while checking every 100 mS for SPEAKING message.
-  while (playWav1.isPlaying()) {
+  while (playSdWav1.isPlaying()) {
     // uncomment these lines if you audio shield
     // has the optional volume pot soldered
     //float vol = analogRead(15);
@@ -152,10 +222,19 @@ int playFileInterruptable(const char *filename)
     
     if (incoming_byte == SPEAKING){
       Serial.println("speaking found in play");
-      playWav1.stop();
+      playSdWav1.stop();
       return_code = 1;
       break;
     }
+
+//    if (rms1.available()) {
+//      int signal_level = 0;
+//      signal_level = rms1.read() * 100.0;
+//      //Serial.print("signal level ");
+//      //Serial.println(signal_level);
+//      if (signal_level > SIG_LEVEL_RANGE){signal_level = SIG_LEVEL_RANGE;}
+//      Serial1.write(signal_level + SIG_LEVEL_MIN);
+//    }
     
     delay(100);
   }
@@ -194,16 +273,18 @@ void loop() {
 //        theme_step = 0;
 //      }
       
-//      playFile(radio[radio_step]);
+//      play_status = playFileInterruptable(radio[radio_step]);
 //      radio_step++;
 //      if (radio_step >= 8) {
 //        radio_step = 0;
 //      }
-//      playFile(theme[theme_step]);
-//      theme_step++;
-//      if (theme_step >= 13) {
-//        theme_step = 0;
+
+//      play_status = playFileInterruptable(jelly_talk[talk_step]);
+//      talk_step++;
+//      if (talk_step >= 20) {
+//        talk_step = 0;
 //      }
+      
       
       if (play_status == 1) {;
         listen_timeout = millis() + LISTEN_WAIT_TIME;
@@ -215,7 +296,7 @@ void loop() {
 
     case SPEAKING:
       sgtl5000_1.volume(0.8);
-      playFile(moon_talk[talk_step]);
+      playTalkFile(moon_talk[talk_step]);
       talk_step++;
       if (talk_step >= 19) {
         talk_step = 0;
@@ -270,7 +351,7 @@ void loop() {
 
   case SPEAKING:
       Serial1.write(SPEAKING);
-      playFile(jelly_talk[talk_step]);
+      playTalkFile(jelly_talk[talk_step]);
         talk_step++;
         if (talk_step >= 20) {
         talk_step = 0;
